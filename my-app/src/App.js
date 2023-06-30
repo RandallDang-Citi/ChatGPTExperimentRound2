7bd4948f70c385ed7ac506a2b5ac32cc720350ba
Randall Dang <82141536+RandallDang-Citi@users.noreply.github.com>
2023-06-30 11:21:48 +0800

    TY74472
    Pyament dashboard page

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/home/home';
+
+// support mutiple themes by ant design
+// import 'antd/dist/antd.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
